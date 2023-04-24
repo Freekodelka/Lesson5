ab21ec41f4bae03012e1a81a3f87c4defde46390
unknown <freekodelka86@gmail.com>
2023-04-24 16:45:30 +0300

    решены задачи 34 и 36

---
+++ Task34/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Задайте массив заполненный случайными положительными трёхзначными числами. 
+ Напишите программу, которая покажет количество чётных чисел в массиве.*/
+int size = 20;
+int[] numbers = new int[size];
+
+void FillArray()
+{
+    Random rnd = new Random();
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = rnd.Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"{numbers[i]} ");
+    }
+    Console.WriteLine('\n');
+}
+
+
+int CountNumbers(int[]numbers)
+{
+    int count = 0;
+    
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] %2 ==0)
+        {
+            count++;
+        }        
+    }
+    return count;
+   
+}
+
+
+FillArray();
+PrintArray(numbers);
+Console.Write("Количество четных чисел: " + CountNumbers(numbers));
+
---
+++ Task36/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов с нечётными индексами.*/
+
+int size = 10;
+int[] numbers = new int[size];
+
+void FillArray()
+{
+    Random rnd = new Random();
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = rnd.Next(0, 100);
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"{numbers[i]} ");
+    }
+    Console.WriteLine('\n');
+}
+
+int SumIndex(int[] numbers)
+{
+    int sum = 0;
+    for (int i = 1; i < numbers.Length; i+=2)
+    {
+        sum += numbers[i];      
+    }
+    return sum;
+}
+
+
+FillArray();
+PrintArray(numbers);
+Console.WriteLine("Сумма чисел под нечетными индексами равна: " + SumIndex(numbers));
